0d2371c57c207c357f369036070ae402238e6d2e
BhusariVedika23 <148196434+BhusariVedika23@users.noreply.github.com>
2026-06-19 23:33:43 +0530

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] ans = new int[amount + 1];
+        ans[0]= 1;
+        for(int coin: coins){
+            for(int i = coin; i <= amount; i++){
+                ans[i] += ans[ i - coin]; 
+            }
+        }
+        return ans[amount];
+    }
+}

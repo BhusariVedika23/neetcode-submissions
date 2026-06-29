b3e554e677837755598c0cf774632a2869146d18
BhusariVedika23 <148196434+BhusariVedika23@users.noreply.github.com>
2026-06-29 20:30:26 +0530

    Add: house-robber - submission-11

---
+++ Data Structures & Algorithms/house-robber/submission-11.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(int[] nums){
+
+        if(nums.length == 0){
+            return 0;
+        }
+        if(nums.length == 1){
+            return nums[0];
+        }
+        int first = nums[0];
+        int second = Math.max(nums[0], nums[1]);
+        for(int i = 2; i<nums.length; i++){
+            int current = Math.max(second, first + nums[i]);
+            first = second;
+            second = current;
+        }
+        return second;
+    }
+}
